47eace53dbd5acc908b756f608a6ca246cd2e44c
chzhxkd100 <chzhxkd100@naver.com>
2025-10-23 18:54:23 +0900

    feat: init

---
+++ src/main/java/com/nhnacademy/chaekmatebatch/ChaekmateBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.chaekmatebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaekmateBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaekmateBatchApplication.class, args);
+    }
+
+}
